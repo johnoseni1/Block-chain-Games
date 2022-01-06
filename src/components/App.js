e7faafaec5fc47228bd346d52a7fd0deab3b229b
John Oseni <johnoseni26@gmail.com>
2022-01-06 15:15:55 +0100

    this is it

--- src/components/App.js
+++ src/components/App.js
@@ -180,15 +180,10 @@ class App extends Component {
     return (
       <div>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+        
           {/* <img src={brain} width="30" height="30" className="d-inline-block align-top" alt="" /> */}
           &nbsp; Memory Tokens
-          </a>
+       
           <ul className="navbar-nav px-3">
             <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
               <small className="text-muted"><span id="account">{this.state.account}</span></small>
